cf80bcfcbb1c4db1219467db863f91889b7b0ed1
Alejandro G. Vera <eldiosvolador@hotmail.com>
2022-05-04 19:15:36 -0300

    Creando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import './index.css'
 import Header from './components/Header'
 
 
 function App() {
-  
+
+  const [presupuesto, setPresupuesto] = useState(0)
+
 
   return (
     <div className="App">
-      <Header />
-    
+      <Header
+        presupuesto = { presupuesto }
+        setPresupuesto = { setPresupuesto }
+      />
+
     </div>
   )
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import { useState } from "react"
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+
+const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault()
+
+    if(!Number(presupuesto)|| Number(presupuesto)<0){
+
+      setMensaje("No es un presupuesto válido")
+    }else{
+      console.log("Es un presupuesto válido")
+    }
+
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
-            <div className='campo'>
-                <label>Definir presupuesto</label>
-                <input type="text"
-                className='nuevo-presupuesto'
-                placeholder='añade tu presupuesto'
-                />
-            </div>
-            <input type="submit" value='añadir'/>
-        </form>
+      <form onSubmit={handlePresupuesto} className='formulario'>
+        <div className='campo'>
+          <label>Definir presupuesto</label>
+          <input type="text"
+            className='nuevo-presupuesto'
+            placeholder='añade tu presupuesto'
+            value={presupuesto}
+            onChange={e => setPresupuesto(e.target.value)}
+          />
+        </div>
+        <input type="submit" value='añadir' />
+      </form>
     </div>
   )
 }
