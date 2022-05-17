e8139b9a23f2a92e9eb142ad37b696434c505ce9
Alejandro G. Vera <eldiosvolador@hotmail.com>
2022-05-17 00:19:25 -0300

    agregando los swipeActions

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,36 +1,51 @@
+import { useState, useEffect } from "react"
 
+const ControlPresupuesto = ({ presupuesto, gastos }) => {
 
-const ControlPresupuesto = ({presupuesto, gastos}) => {
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
 
-const formatearCantidad =(cantidad)=>{
+    useEffect(() => {
 
-    return cantidad.toLocaleString('en-US',{
+        const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
 
-        style:'currency',
-        currency: 'USD'
+        const totatlDisponible = presupuesto - totalGastado
 
-    })
+        setDisponible(totatlDisponible)
+        
+        setGastado(totalGastado)
 
-}
+    }, [gastos])
 
-  return (
-    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-        <div>
-            <p>Grafica</p>
+    const formatearCantidad = (cantidad) => {
+
+        return cantidad.toLocaleString('en-US', {
+
+            style: 'currency',
+            currency: 'USD'
+
+        })
+
+    }
+
+    return (
+        <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+            <div>
+                <p>Grafica</p>
+            </div>
+            <div className="contenido-presupuesto">
+                <p>
+                    <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
+                </p>
+                <p>
+                    <span>Disponible: </span>{formatearCantidad(disponible)}
+                </p>
+                <p>
+                    <span>Gastado: </span>{formatearCantidad(gastado)}
+                </p>
+            </div>
         </div>
-        <div className="contenido-presupuesto">
-            <p>
-                <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
-            </p>
-            <p>
-                <span>Disponible: </span>{formatearCantidad(0)}
-            </p>
-            <p>
-                <span>Gastado: </span>{formatearCantidad(0)}
-            </p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default ControlPresupuesto
